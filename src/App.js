d716ea41380e7dce425e07154c98c86af7e2565f
eriksandviksea <eriksandvikSEA@Gmail.com>
2020-04-24 09:01:18 -0700

    added size select dropdown

--- src/App.js
+++ src/App.js
@@ -1,9 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 import { Route, Switch, Link } from 'react-router-dom'
 import YourOrder from './components/YourOrder'
 import OrderForm from './components/OrderForm'
 
+const initialFormValues = {
+  name: '',
+  size: 'Select Size',
+  toppings: {
+    pepperoni: false,
+    mushroom: false,
+    blackOlive: false,
+    pineapple: false,
+  },
+  specialInstructions: '',
+}
+
+
+
+
+
+
+
+
+
 const App = () => {
+  const [formValues, setFormValues] = useState(initialFormValues)
+
+
+
+  const onInputChange = evt => {
+    const inputName = evt.target.name
+    const inputValue = evt.target.value
+
+    setFormValues({
+      ...formValues,
+      [inputName]: inputValue
+    })
+  }
+
+
+
+
+
+
   return (
     <>
       <header>
@@ -23,7 +62,11 @@ const App = () => {
 
 
       <Route path='/pizza'>
-        <OrderForm />
+        <OrderForm
+          values={formValues}
+          onInputChange={onInputChange}
+         
+         />
       </Route>
     </Switch>
 
--- src/components/OrderForm.jsx
+++ src/components/OrderForm.jsx
@@ -2,9 +2,40 @@ import React, { useState, useEffect } from 'react'
 import { useParams, useHistory, useRouteMatch, Route, Link } from 'react-router-dom' // hook
 
 
-const OrderForm = () => {
+const OrderForm = (props) => {
+     const {
+          values,
+          onInputChange,
+     } = props
      return (
-          <div>Build Your Order</div>
+          <div>
+               <h1>
+                    Build Your Order
+               </h1>
+          
+          <form>
+               <label>Enter Name:&nbsp;
+                    <input 
+                         value={values.name}
+                         onChange={onInputChange}
+                         name='name'
+                         type='text'
+                    />
+               </label>
+               <label>Size:&nbsp;
+       <select
+                              // very similar to inputs of type text
+                              value={values.size}
+                              onChange={onInputChange}
+                              name='size'
+                         >
+                              <option value='selectSize'>Select Size</option>
+                              <option value='small'>Small</option>
+                              <option value='medium'>Medium</option>
+                              <option value='large'>Large</option>
+                         </select></label>
+          </form>
+          </div>
      )
      }
 
