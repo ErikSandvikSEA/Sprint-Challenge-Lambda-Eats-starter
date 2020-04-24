e55d5d6e535d2e911943d61b5fe6dea4be6202dd
eriksandviksea <eriksandvikSEA@Gmail.com>
2020-04-24 09:56:38 -0700

    name form validation - 3 chars

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React, { useState } from "react";
 import { Route, Switch, Link } from 'react-router-dom'
 import YourOrder from './components/YourOrder'
 import OrderForm from './components/OrderForm'
+import * as yup from 'yup'
+
 
 const initialFormValues = {
   name: '',
-  size: 'Select Size',
+  size: 'selectSize',
   toppings: {
     pepperoni: false,
     mushroom: false,
@@ -15,7 +17,16 @@ const initialFormValues = {
   specialInstructions: '',
 }
 
+const initialFormErrors = {
+  name: '',
+}
 
+const formSchema = yup.object().shape({
+  name: yup
+    .string()
+    .required()
+    .min(3, 'Name must have at least 3 characters'),
+})
 
 
 
@@ -25,6 +36,7 @@ const initialFormValues = {
 
 const App = () => {
   const [formValues, setFormValues] = useState(initialFormValues)
+  const [formErrors, setFormErrors] = useState(initialFormErrors)
 
 
 
@@ -32,13 +44,41 @@ const App = () => {
     const inputName = evt.target.name
     const inputValue = evt.target.value
 
+    yup
+      .reach(formSchema, inputName)
+      .validate(inputValue)
+      .then(valid => {
+        //clear errors
+        setFormErrors({
+          ...formErrors,
+          [inputName]: '',
+        })
+      })
+      .catch(err => {
+        setFormErrors({
+          ...formErrors,
+          [inputName]: err.errors[0]
+        })
+      })
+
     setFormValues({
       ...formValues,
       [inputName]: inputValue
     })
   }
 
+  const onCheckboxChange = evt => {
+    const { name } = evt.target
+    const isChecked = evt.target.checked
 
+    setFormValues({
+      ...formValues,
+      toppings: {
+        ...formValues.hobbies,
+        [name]: isChecked,
+      }
+    })
+  }
 
 
 
@@ -65,6 +105,8 @@ const App = () => {
         <OrderForm
           values={formValues}
           onInputChange={onInputChange}
+          onCheckboxChange={onCheckboxChange}
+          errors={formErrors}
          
          />
       </Route>
--- src/components/OrderForm.jsx
+++ src/components/OrderForm.jsx
@@ -6,23 +6,42 @@ const OrderForm = (props) => {
      const {
           values,
           onInputChange,
+          onCheckboxChange,
+          errors
      } = props
      return (
           <div>
                <h1>
                     Build Your Order
                </h1>
-          
-          <form>
-               <label>Enter Name:&nbsp;
-                    <input 
-                         value={values.name}
-                         onChange={onInputChange}
-                         name='name'
-                         type='text'
-                    />
-               </label>
-               <label>Size:&nbsp;
+
+               <form>
+                    <div className='errors'>
+                         <h3
+                              data-cyName='nameErrorInput'
+                              value={errors.name}>{errors.name}</h3>
+
+                         <h3
+                              data-cyName='emailErrorInput'
+                              value={errors.email}>{errors.email}</h3>
+
+                         <h3
+                              data-cyName='passwordErrorInput'
+                         >{errors.password}</h3>
+
+                         <h3>{errors.termsOfService}</h3>
+
+                    </div>
+
+                    <label>Enter Name:&nbsp;
+                    <input
+                              value={values.name}
+                              onChange={onInputChange}
+                              name='name'
+                              type='text'
+                         />
+                    </label>
+                    <label>&nbsp;Size:&nbsp;
        <select
                               // very similar to inputs of type text
                               value={values.size}
@@ -34,9 +53,40 @@ const OrderForm = (props) => {
                               <option value='medium'>Medium</option>
                               <option value='large'>Large</option>
                          </select></label>
-          </form>
+
+                    <label><input
+                         checked={values.toppings.pepperoni}
+                         onChange={onCheckboxChange}
+                         name='pepperoni'
+                         type="checkbox" /> Pepperoni</label>
+                    <label><input
+                         checked={values.toppings.mushroom}
+                         onChange={onCheckboxChange}
+                         name='mushroom'
+                         type="checkbox" /> Mushroom</label>
+                    <label><input
+                         checked={values.toppings.blackOlive}
+                         onChange={onCheckboxChange}
+                         name='blackOlive'
+                         type="checkbox" /> Black Olive</label>
+                    <label><input
+                         checked={values.toppings.pineapple}
+                         onChange={onCheckboxChange}
+                         name='pineapple'
+                         type='checkbox' /> Pineapple
+        </label>
+
+                    <label>&nbsp;Special Instructions:&nbsp;
+                    <input
+                              value={values.specialInstructions}
+                              onChange={onInputChange}
+                              name='specialInstructions'
+                              type='text'
+                         />
+                    </label>
+               </form>
           </div>
      )
-     }
+}
 
-     export default OrderForm
+export default OrderForm
